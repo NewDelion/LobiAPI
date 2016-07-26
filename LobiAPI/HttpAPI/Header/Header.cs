4e00df62f00f08e22478f846bc886ff4c8e28227
NewDelion <el.emena.crea@gmail.com>
2016-07-26 17:26:45 +0900

    BasicHeaderを削除

--- LobiAPI/HttpAPI/Header/Header.cs
+++ LobiAPI/HttpAPI/Header/Header.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace LobiAPI.HttpAPI.Header
 {
-    public class BasicHeader
+    public class GetHeader
     {
         public string Host;
         public bool Connection = true;
@@ -16,50 +16,7 @@ namespace LobiAPI.HttpAPI.Header
         public string AcceptEncoding;
         public string AcceptLanguage;
         public string Origin;
-
-        public BasicHeader setHost(string host)
-        {
-            this.Host = host;
-            return this;
-        }
-        public BasicHeader setConnection(bool connection)
-        {
-            this.Connection = connection;
-            return this;
-        }
-        public BasicHeader setAccept(string accept)
-        {
-            this.Accept = accept;
-            return this;
-        }
-        public BasicHeader setUserAgent(string useragent)
-        {
-            this.UserAgent = useragent;
-            return this;
-        }
-        public BasicHeader setReferer(string referer)
-        {
-            this.Referer = referer;
-            return this;
-        }
-        public BasicHeader setAcceptEncoding(string acceptencoding)
-        {
-            this.AcceptEncoding = acceptencoding;
-            return this;
-        }
-        public BasicHeader setAcceptLanguage(string acceptlanguage)
-        {
-            this.AcceptLanguage = acceptlanguage;
-            return this;
-        }
-        public BasicHeader setOrigin(string origin)
-        {
-            this.Origin = origin;
-            return this;
-        }
-    }
-    public class GetHeader : BasicHeader
-    {
+        
         public GetHeader setHost(string host)
         {
             this.Host = host;
@@ -101,8 +58,16 @@ namespace LobiAPI.HttpAPI.Header
             return this;
         }
     }
-    public class PostHeader : BasicHeader
+    public class PostHeader
     {
+        public string Host;
+        public bool Connection = true;
+        public string Accept;
+        public string UserAgent;
+        public string Referer;
+        public string AcceptEncoding;
+        public string AcceptLanguage;
+        public string Origin;
         public string ContentType;
 
         public PostHeader setHost(string host)
